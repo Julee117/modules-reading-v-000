13f68e18194a977c9af9fdbd528859e64ec3f539
Julianne Kim <juliannne@gmail.com>
2017-10-23 22:03:04 +0000

    automatically backed up by learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,9 +1,13 @@
 module Dance
-  def twirl 
+  def twirl
     "I'm twirling!"
   end
 
   def jump
     "Look how high I'm jumping!"
+  end
+
+  def pirouette
+    "I'm doing a pirouette"
   end
 end
